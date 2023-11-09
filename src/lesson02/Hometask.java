7ed546fe90ce6a483d037b70e35016d1a4471ffb
User <uzvarykoleksandr@gmail.com>
2023-11-10 01:37:05 +0200

    lesson1

---
+++ src/lesson02/Hometask.java
@@ -0,0 +1,49 @@
+package lesson02;
+    public class Hometask {
+        public static void main(String[] args) {
+
+            byte age = 45;
+            short numberOfChildren = 3;
+            int losses = -8756;
+            long followers = 1002145;
+            float power = 15.878f;
+            double energyLevel = 15.5;
+            boolean respect = true;
+            char firstLetter = 'M';
+
+            System.out.println("Task2 'Primitive values':");
+            System.out.println(age);
+            System.out.println(numberOfChildren);
+            System.out.println(losses);
+            System.out.println(followers);
+            System.out.println(power);
+            System.out.println(energyLevel);
+            System.out.println(respect);
+            System.out.println(firstLetter);
+
+
+            int numberAbc = 555;
+            int a = (numberAbc -50) / 100;
+            int b = numberAbc*2/222;
+            int c = (numberAbc/5) - 106;
+
+            System.out.println("Task3 'All variables':");
+            System.out.println("My number is :" + numberAbc);
+
+            System.out.println("a-" + a);
+            System.out.println("b-" + b);
+            System.out.println("c-" +c);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
