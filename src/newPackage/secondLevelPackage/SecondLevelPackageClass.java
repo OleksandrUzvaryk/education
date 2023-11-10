754b9adcdb16b3f1012e704a065a24053fd3a029
User <uzvarykoleksandr@gmail.com>
2023-11-10 10:20:48 +0200

    lesson1

---
+++ src/newPackage/secondLevelPackage/SecondLevelPackageClass.java
@@ -0,0 +1,7 @@
+package newPackage.secondLevelPackage;
+
+public class SecondLevelPackageClass {
+
+    // полный путь который видит джава для моего класса  SecondLevelPackageClass
+    // newPackage/secondLevelPackage/SecondLevelPackageClass.java
+}
