55430b6032a9c640dac81ae51cd7bd99adda4480
User <uzvarykoleksandr@gmail.com>
2023-11-22 11:46:53 +0200

    lesson1

--- src/lesson07/hometask/task1degree/NumberDegree.java
+++ src/lesson07/hometask/task1degree/NumberDegree.java
@@ -4,7 +4,7 @@ public class NumberDegree {
     public static void main(String[] args) {
         int number = 3;
         int degree = 3;
-        //double result = Math.pow(x,y);
+        //double result = Math.pow(number,degree);
         int degree1 = number;//3
         int degree2 = number*degree1;//9
         int degree3 = number*degree2;//27
